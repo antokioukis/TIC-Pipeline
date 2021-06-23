d22931d6ba5bbfd4697d693e9c0f5235c686941b
antonios <antokioukis@gmail.com>
2021-06-23 09:12:33 +0300

    process single sequencing

--- 1.ASV-Creation/process_samples.py
+++ 1.ASV-Creation/process_samples.py
@@ -1,5 +1,19 @@
 from sys import argv
 from os import system, remove, chdir, getcwd
+from statistics import stdev, mean
+
+
+CLUSTERING_TOOL = argv[1]
+MAXDIFF = argv[2]
+USER_FASTQ_FOLDER = argv[3]
+TRIM_SCORE = argv[4]
+MINMERGELEN = argv[5]
+MAXMERGELEN = argv[6]
+FORWARD_TRIM = argv[7]
+REVERSE_TRIM = argv[8]
+EXPECTED_ERROR_RATE = argv[9]
+THREADS = argv[10]
+MINPCTID = argv[11]
 
 
 # return list with each line an element, clear of whitespace
@@ -20,16 +34,31 @@ def fastq_merge_pairs(forward_file, reverse_file):
 
 def trimming():
     cmd = CLUSTERING_TOOL + " -fastx_truncate " + USER_FASTQ_FOLDER + '/merged.fasta -stripleft '
-    cmd += FORWARD_TRIM + ' -stripright ' + REVERSE_TRIM + ' -fastqout ' + USER_FASTQ_FOLDER + '/trimmed.fasta '
+    cmd += FORWARD_TRIM + ' -stripright ' + REVERSE_TRIM + ' -fastqout ' + USER_FASTQ_FOLDER + '/trimmed.fastq '
     cmd += '2>>' + USER_FASTQ_FOLDER + '/log_file.txt' + ' 1>>' + USER_FASTQ_FOLDER + '/log_file.txt'
     system(cmd)
+
+
+def trim_one_side(forward_file):
+    cmd_0 = CLUSTERING_TOOL + " -fastx_truncate " + forward_file + " -stripleft " + FORWARD_TRIM
+    cmd_1 = " -fastqout " + USER_FASTQ_FOLDER + "/trimmed.fastq >/dev/null 2>/dev/null"
+    system(cmd_0 + cmd_1)
 
 
 def filtering():
-    cmd = CLUSTERING_TOOL + " -fastq_filter " + USER_FASTQ_FOLDER + '/trimmed.fasta --fastq_maxee_rate '
+    cmd = CLUSTERING_TOOL + " -fastq_filter " + USER_FASTQ_FOLDER + '/trimmed.fastq --fastq_maxee_rate '
     cmd += EXPECTED_ERROR_RATE + ' -fastaout ' + USER_FASTQ_FOLDER + '/filtered.fasta '
     cmd += '2>>' + USER_FASTQ_FOLDER + '/log_file.txt' + ' 1>>' + USER_FASTQ_FOLDER + '/log_file.txt'
     system(cmd)
+
+
+def filter_merged_one_side(forward_file):
+    curr_mean, curr_sd = seqFileStats(forward_file)
+    minLength = curr_mean - int(0.1*curr_mean) - int(FORWARD_TRIM)  # remove the primer triming size plus 10% of the mean size
+    cmd_0 = CLUSTERING_TOOL + ' -fastq_filter trimmed.fastq -fastq_truncqual 20'
+    cmd_1 = ' -fastq_maxee_rate ' + EXPECTED_ERROR_RATE + ' -fastq_trunclen ' + str(minLength)
+    cmd_2 = ' -fastaout filtered.fasta >/dev/null 2>/dev/null'
+    system(cmd_0 + cmd_1 + cmd_2)
 
 
 def dereplication():
@@ -39,47 +68,89 @@ def dereplication():
     system(cmd)
 
 
+def seqFileStats(seqFileName):
+    try:
+        seqfile_fh = read_file(seqFileName)
+    except BaseException:
+        print("Cannot open " + seqFileName + " to read from.")
+        exit()
+    lengths = list()
+    for i in range(len(seqfile_fh)):
+        if not seqfile_fh[i]:
+            continue
+        if seqfile_fh[i][0] == '@':
+            curr_len = len(seqfile_fh[i+1])
+            lengths.append(curr_len)
+    mean_length = int(mean(lengths))
+    st_dev = int(stdev(lengths))
+    return(mean_length, st_dev)
+
+
 def process_samples():
     top_directory = getcwd()
     chdir(USER_FASTQ_FOLDER)
+    reverse_file = ''
     tab_file_contents = read_file('mapping_file.ssv')
     for line in tab_file_contents:
         tokens = line.split()
         sample_name = tokens[0]
         forward_file = tokens[2]
-        if tokens[1] == '1':
+        if tokens[1] == '2':
             reverse_file = tokens[3]
-        print('>>> Processing Sample:' + sample_name)
-        out_file = open(top_directory + '/log_file.txt', 'a+')
-        out_file.write('\n')
-        out_file.write('>>> Processing Sample:' + sample_name + '\n')
-        out_file.write('-----------------------------------\n')
-        out_file.close()
-        print('\tMerging...')
-        try:
-            fastq_merge_pairs(forward_file, reverse_file)
-        except BaseException:
-            print('Merging command failed with a critical failure. Exiting...\n')
-            exit(1)
-        print("\tMerging pairs " + forward_file + ' and ' + reverse_file + ' from sample ' + sample_name + '... Done')
-        system('cat ' + USER_FASTQ_FOLDER + '/report.txt >> ' + top_directory + '/log_file.txt')
-        remove(USER_FASTQ_FOLDER + '/report.txt')
-        print('\tTrimming...')
-        try:
-            trimming()
-        except BaseException:
-            print('Trimming command failed with a critical failure. Exiting...\n')
-            exit(1)
-        else:
-            print('\tDone')
-        print('\tFiltering merged reads...')
-        try:
-            filtering()
-        except BaseException:
-            print('Filtering command failed. Exiting...\n')
-            exit(1)
-        else:
-            print('\tDone')
+            print('>>> Processing Sample:' + sample_name)
+            out_file = open(top_directory + '/log_file.txt', 'a+')
+            out_file.write('\n')
+            out_file.write('>>> Processing Sample:' + sample_name + '\n')
+            out_file.write('-----------------------------------\n')
+            out_file.close()
+            print('\tMerging...')
+            try:
+                fastq_merge_pairs(forward_file, reverse_file)
+            except BaseException:
+                print('Merging command failed with a critical failure. Exiting...\n')
+                exit(1)
+            print("\tMerging pairs " + forward_file + ' and ' + reverse_file + ' from sample ' + sample_name + '... Done')
+            system('cat ' + USER_FASTQ_FOLDER + '/report.txt >> ' + top_directory + '/log_file.txt')
+            remove(USER_FASTQ_FOLDER + '/report.txt')
+            print('\tTrimming...')
+            try:
+                trimming()
+            except BaseException:
+                print('Trimming command failed with a critical failure. Exiting...\n')
+                exit(1)
+            else:
+                print('\tDone')
+            print('\tFiltering merged reads...')
+            try:
+                filtering()
+            except BaseException:
+                print('Filtering command failed. Exiting...\n')
+                exit(1)
+            else:
+                print('\tDone')
+        elif tokens[1] == '1':
+            print('>>> Processing Sample:' + sample_name)
+            out_file = open(top_directory + '/log_file.txt', 'a+')
+            out_file.write('\n')
+            out_file.write('>>> Processing Sample:' + sample_name + '\n')
+            out_file.write('-----------------------------------\n')
+            out_file.close()
+            print('\tTrimming...')
+            try:
+                trim_one_side(forward_file)
+            except BaseException:
+                print('Trimming command failed with a critical failure. Exiting...\n')
+                exit(1)
+            else:
+                print('\tDone')
+            print('\tFiltering...')
+            try:
+                filter_merged_one_side(forward_file)
+            except BaseException:
+                print('Filtering command failed. Exiting...\n')
+                exit(1)
+            else:
+                print('\tDone')
         print('\tDereplicating sequences...')
         try:
             dereplication()
@@ -89,22 +160,19 @@ def process_samples():
         else:
             print('\tDone')
         system('mv unique.fasta ' + sample_name + '_unique.fasta')
+    try:
         remove('merged.fasta')
+    except BaseException:
+        pass
+    try:
         remove('trimmed.fasta')
+    except BaseException:
+        pass
+    try:
         remove('filtered.fasta')
+    except BaseException:
+        pass
     chdir(top_directory)
 
-
-CLUSTERING_TOOL = argv[1]
-MAXDIFF = argv[2]
-USER_FASTQ_FOLDER = argv[3]
-TRIM_SCORE = argv[4]
-MINMERGELEN = argv[5]
-MAXMERGELEN = argv[6]
-FORWARD_TRIM = argv[7]
-REVERSE_TRIM = argv[8]
-EXPECTED_ERROR_RATE = argv[9]
-THREADS = argv[10]
-MINPCTID = argv[11]
 
 process_samples()
